e1463929e72cd8454168c0d378f7cb5170c37d3b
Antonio Guedes <gguedesantonio@gmail.com>
2020-06-01 18:21:53 +1000

    Debuging of typos

--- script.js
+++ script.js
@@ -3,44 +3,46 @@ let generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  // Prompts for password criteria
-  let passwordLenght = prompt("How long do you want your password?")
-  // ADD CHECKER FOR PASSWORD LENGHT CRITERIA******************************
-  let passwordCase = confirm("Does your password needs uppercase letters?");
-  let passwordNumbers = confirm("Does your password needs numbers?");
-  let passwordSpecial = confirm("Does your password needs special characters?")
+
   let password = generatePassword();
   let passwordText = document.querySelector('#password');
-// *******************CREATE QUESTION PASSWORD LENGHT*******************
+
   passwordText.value = password;
 }
 // generate random password function
 function generatePassword(){
+    // Prompts for password criteria
+    let passwordth = prompt("How long do you want your password?")
+    // ADD CHECKER FOR PASSWORD LENGHT CRITERIA******************************
+    let passwordCase = confirm("Does your password needs uppercase letters?");
+    let passwordNumbers = confirm("Does your password needs numbers?");
+    let passwordSpecial = confirm("Does your password needs special characters?")
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 
-  if (passwordCase===true && passwordNumbers===true && passwordSpecial===true) {
+  if (passwordCase === true && passwordNumbers === true && passwordSpecial === true) {
     let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
   }
-  else if (passwordCase===true && passwordNumbers===true && passwordSpecial===false) {
+  else if (passwordCase === true && passwordNumbers === true && passwordSpecial === false) {
     let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
   }
-  else if (passwordCase===true && passwordNumbers===false && passwordSpecial===true) {
+  else if (passwordCase === true && passwordNumbers === false && passwordSpecial === true) {
     let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+";
   }
-  else if (passwordCase===false && passwordNumbers===true && passwordSpecial===false) {
+  else if (passwordCase === false && passwordNumbers === true && passwordSpecial === false) {
     let values = "abcdefghijklmnopqrstuvwxyz1234567890";
   }
-  else if (passwordCase===false && passwordNumbers===false && passwordSpecial===true) {
+  else if (passwordCase === false && passwordNumbers === false && passwordSpecial === true) {
     let values = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+";
   }
-  else if (passwordCase===false && passwordNumbers===false && passwordSpecial===false) {
+  else if (passwordCase === false && passwordNumbers === false && passwordSpecial === false) {
     let values = "abcdefghijklmnopqrstuvwxyz";
   }
 
   let password = "";
 
   // loop to create random characters
-  for (let i = 0;i<=passwordLenght;i++){
-    password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.lenght - 1)));
+  for (let i = 0; i <= passwordLength; i++){
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
   }
 
   return password;
