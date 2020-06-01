612f97aa011db62d0dfca5c0bae7f5ffb8fbb75f
Antonio Guedes <gguedesantonio@gmail.com>
2020-06-01 16:37:15 +1000

    Prompt logic done

--- script.js
+++ script.js
@@ -3,10 +3,33 @@ let generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
+  // Prompts for password criteria
+  let passwordLenght = prompt("How long do you want your password?")
+  // ADD CHECKER FOR PASSWORD LENGHT CRITERIA******************************
+  let passwordCase = confirm("Does your password needs uppercase letters?");
+  let passwordNumbers = confirm("Does your password needs numbers?");
+  let passwordSpecial = confirm("Does your password needs special characters?")
   let password = generatePassword();
   let passwordText = document.querySelector('#password');
-
+// *******************CREATE QUESTION PASSWORD LENGHT*******************
   passwordText.value = password;
+}
+// generate random password function
+function generatePassword(){
+  if (passwordCase===true) {
+
+
+    
+  }
+  let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+  let password = "";
+
+  // loop to create random characters
+  for (let i = 0;i<=passwordLenght;i++){
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.lenght - 1)));
+  }
+
+  return password;
 }
 
 // Add event listener to generate button
